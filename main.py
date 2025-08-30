97a5d1b5e4c256d6777cf73f1609176a55403c3f
REFIORE <aras_andrey@bk.ru>
2025-08-30 18:56:11 +0300

    шаг 6

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import os
+import os.path
 from cryptography.fernet import Fernet
-
-key = Fernet.generate_key()
-fer = Fernet(key)
 
 
 def write_key():
+    key = Fernet.generate_key()
     with open("key.key", "wb") as f:
         f.write(key)
 
 
 def load_key():
-    with open("key.key", "r") as f:
-        text = f.read(key)
-        print(text)
+    with open("key.key", "rb") as text:
+        password = text.read()
+    return password
 
 
 def main():
-    write_key()
-    load_key()
+    if not os.path.exists("key.key"):
+        write_key()
+    key = load_key()
+    fer = Fernet(key)
 
 
 if __name__ == '__main__':
