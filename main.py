376579aadcdf23e10201355820f346514a466a66
REFIORE <aras_andrey@bk.ru>
2025-08-30 19:20:49 +0300

    Шаг 7

--- main.py
+++ main.py
@@ -14,11 +14,20 @@ def load_key():
     return password
 
 
+def add(fer):
+    login = input("Введите логин:")
+    password = input("Введите пароль:")
+    with open("passwords.txt", "w") as file:
+        encrypted_password = fer.encrypt(password.encode()).decode()
+        file.write(f"{login}|{encrypted_password}\n")
+
+
 def main():
     if not os.path.exists("key.key"):
         write_key()
     key = load_key()
     fer = Fernet(key)
+    add(fer)
 
 
 if __name__ == '__main__':
